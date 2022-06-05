bcc4ae372ffa0345e00a55a209c8e8a9eed1df99
Tauno Erik <sedumacre@gmail.com>
2022-06-05 21:24:13 +0300

    new funcs

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,13 @@
+/************************************************
+ * File: main.cpp
+ * Project: Lillering
+ * MCU Board: Arduino Pro Micro (BTE13-010B, www.betemcu.cn) 16MHz atmega328 5V
+ * Github: https://github.com/taunoe/lillering
+ * 
+ * Last edited: 04.06.2022
+ * 
+ * Copyright 2022 Tauno Erik
+ ************************************************/
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
 
@@ -17,10 +27,158 @@ const uint8_t PIXEL_PIN = 10;
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
 Adafruit_NeoPixel RGB = Adafruit_NeoPixel(1, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
+// Arduino pin names
+const uint8_t LATCH_PIN =  8;  // PB0;
+const uint8_t CLOCK_PIN = 12;  // PB4;
+const uint8_t DATA_PIN  = 11;  // PB3;
 
-const uint8_t LATCH_PIN =  8;
-const uint8_t CLOCK_PIN = 12;
-const uint8_t DATA_PIN  = 11;
+// Atmega PortB pin names
+const uint8_t PORTB_LATCH_PIN = 0;  // PB0;
+const uint8_t PORTB_CLOCK_PIN = 4;  // PB4;
+const uint8_t PORTB_DATA_PIN = 3;   // PB3;
+
+const uint8_t NUM_7SEGS = 5;
+
+uint8_t data[NUM_7SEGS] = {
+  0b01010101,
+  0b01010101,
+  0b01010101,
+  0b01010101,
+  0b01010101
+};
+
+/* Shift Register Functions */
+void all_off(){
+  for (uint8_t i = 0; i < NUM_7SEGS; i++){
+    data[i] = 0;
+  }
+}
+
+// Väljasta kogu data massiivi korraga
+void output_all() {
+  for (uint8_t i = 0; i < NUM_7SEGS; i++) {
+    digitalWrite(LATCH_PIN, LOW);
+    shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, data[i]);
+    digitalWrite(LATCH_PIN, HIGH);
+  }
+}
+
+/*
+Shift data to 7-segments.
+*/
+
+void shift_out_all_data(uint8_t data[]) {
+  for (uint8_t segment = 0; segment < NUM_7SEGS; segment++) {
+
+    PORTB &= ~(1<<PORTB_LATCH_PIN);  // digitalWrite(LATCH_PIN, LOW);
+
+    uint8_t d = data[segment];
+
+    for (uint8_t i = 0; i < 8; i++) {
+      uint8_t bit = (d & 128) != 0;
+      if (bit == 0) {
+        PORTB &= ~(1 << PORTB_DATA_PIN);
+      } else {
+        PORTB |= (1 << PORTB_DATA_PIN);
+      }
+      d <<= 1;
+
+      PORTB |= (1<<PORTB_CLOCK_PIN);   // Set to HIGH
+      PORTB &= ~(1<<PORTB_CLOCK_PIN);  // Set to LOW
+    }
+    
+    PORTB |= (1<<PORTB_LATCH_PIN);  // digitalWrite(LATCH_PIN, HIGH);
+  }
+}
+
+/*
+  One by one: CounterClockWise
+ */
+void one_by_one_CCW(uint32_t delay_time) {
+  all_off();
+
+  for (uint8_t seg = NUM_7SEGS; seg > 0; seg--) {
+    data[seg-1] = 1;
+    shift_out_all_data(data);
+    delay(delay_time);
+
+    for (uint8_t bit = 0; bit < 8; bit++) {
+      data[seg-1] = (data[seg-1]<<1);
+      shift_out_all_data(data);
+      delay(delay_time);
+    }
+
+    data[seg-1] = 0;
+    shift_out_all_data(data);
+  }
+}
+
+/*
+  One by one: ClockWise
+ */
+void one_by_one_CW(uint32_t delay_time) {
+  all_off();
+
+  for (uint8_t seg = 0; seg < NUM_7SEGS; seg++) {
+    data[seg] = 0b10000000;
+    shift_out_all_data(data);
+    delay(delay_time);
+
+    for (uint8_t bit = 0; bit < 8; bit++) {
+      data[seg] = (data[seg]>>1);
+      shift_out_all_data(data);
+      delay(delay_time);
+    }
+    data[seg] = 0;
+    shift_out_all_data(data);
+  }
+}
+
+const uint8_t COLS[8] = {
+  0b10000000,
+  0b01000000,
+  0b00100000,
+  0b00010000,
+  0b00001000,
+  0b00000100,
+  0b00000010,
+  0b00000001
+};
+
+
+void one_by_one_in_two_directions(uint32_t delay_time) {
+  all_off();
+
+  for (uint8_t r = 0; r < NUM_7SEGS; r++) {
+
+    for (size_t c = 0; c < 8; c++) {
+      if ((c-1) > 7) {
+        // a
+        data[r] &= ~(COLS[7]);      // set back to Low
+        // b
+        data[4] &= ~(COLS[0]);      // set back to LOW
+      } else {
+        // a
+        data[r] &= ~(COLS[c-1]);    // set back to Low
+        // b
+        data[4] &= ~(COLS[7-c+1]);  // set back to LOW
+      }
+
+      // a
+      data[r] |= COLS[c];           // set to High
+      // b
+      data[4] |= COLS[7-c];         // set to High
+
+      shift_out_all_data(data);     // Display
+      delay(delay_time);            // Wait
+    }
+  
+  }
+
+}
+
+
+/* NeoPixel Functions */
 
 // Fill the dots one after the other with a color
 void colorWipe(uint32_t c, uint8_t wait) {
@@ -45,18 +203,15 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("loop");
-
   colorWipe(RGB.Color(255, 0, 0), 50); // Red
 
+  // One by one: CounterClockWise
+  // one_by_one_CCW(500);
 
-  digitalWrite(LATCH_PIN, LOW);
-  shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, 0b00000001);
-  digitalWrite(LATCH_PIN, HIGH);
+  // One by one: ClockWise
+  // one_by_one_CW(10);
 
-  delay(1000);
-  digitalWrite(LATCH_PIN, LOW);
-  shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, 0b00000000);
-  digitalWrite(LATCH_PIN, HIGH);
-  delay(1000);
+  //
+  one_by_one_in_two_directions(2000);
+
 }
